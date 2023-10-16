30bc3e8a86f87d23092314bdc1491939264920b5
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-10-16 10:28:20 +0600

    cartering and ohters

--- src/Component/CateringServices.js
+++ src/Component/CateringServices.js
@@ -1,15 +1,51 @@
+import cake from '@/images/catarning/cake.jpg';
+import food from '../images/catarning/food_.png'
+import meeting from '../images/catarning/meeting.png'
+import Image from 'next/image';
 
 
 const CateringServices = () => {
     return (
-        <div>
+        <div className='my-10'>
             <h1 className="lg:text-6xl lg:font-extrabold text-center">Catering Services</h1>
-            <div>
-                <div>
+            <div className='flex justify-around'>
+                <div className='text-center'>
+                    <Image
+                        className='w-20 h-22 ml-24'
+  src={cake}
 
+  alt="picture about "
+/>
+                    <h3 className='text-3xl lg:font-bold'>Birthday Party</h3>
+                    <p className='w-64 text-center'>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.
+
+</p>
                 </div>
-                <div></div>
-                <div></div>
+                <div className='text-center'>
+                   <Image
+                        className='w-32  ml-24'
+  src={meeting}
+
+  alt="picture about "
+/>
+                    <h3 className='text-3xl lg:font-bold'>Birthday Party</h3>
+                    <p className='w-64 text-center'>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.
+
+</p>
+                </div>
+                <div className='text-center'>
+                    <Image
+                        className='w-20 h-22'
+  src={food}
+
+  alt="picture about "
+/>
+                    <h3 className='text-3xl lg:font-bold'>Birthday Party</h3>
+                    <p className='w-64 text-center'>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.
+
+</p>
+                </div>
+                
             </div>
         </div>
     );
