f5a2c8587b7f7b1310881ccf21d525d6ebf03d61
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-10-16 22:27:19 +0600

    chef add

--- src/Component/Home/Master_Chef.js
+++ src/Component/Home/Master_Chef.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import owner from '../../images/chef/male_one.jpg'
+import Image from 'next/image';
+import { LiaTwitter } from 'react-icons/lia';
+import { FaFacebookF } from 'react-icons/fa';
+import { AiOutlineGoogle } from 'react-icons/ai';
+import { BsInstagram } from 'react-icons/bs';
 
 const Master_Chef = () => {
     return (
-        <div>
+        <div className='my-16'>
             <h1 className='text-center text-4xl font-bold'>Our Master Chef</h1>
             <div>
-
+                <div className='w-64  '>
+                    <Image src={owner} className="w-64 shadow-lg " alt="owner" />
+                    <h4 className='text-xl font-bold my-2'>John Smooth</h4>
+                    <p>Restaurant Owner</p>
+                    <div className='flex items-center text-lg mt-3' style={{color:"#c8a97e"}}>
+                        <p className='mr-3'><LiaTwitter/></p>
+                        <p className='mr-3'><FaFacebookF/></p>
+                        <p className='mr-3'><AiOutlineGoogle/></p>
+                        <p className='mr-3'><BsInstagram/></p>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/Component/Home/Menu.js
+++ src/Component/Home/Menu.js
@@ -4,19 +4,22 @@ import ch  from "../../images/menu/chiken_crispy.jpg"
 
 const Menu = () => {
     return (
-        <div className="my-20  container">
+        <div className="my-20  ">
             <h1 className="text-6xl font-extrabold text-center my-7">Our Menu</h1>
-             <div className="grid grid-cols-3 ">
-             <div className="card w-72 bg-base-100 shadow-xl ">
-  <Image src={ch} className="w-72 h-72" alt="Shoes" />
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+             <div className=" ">
+          <div className="  flex items-center rounded-lg border-2 w-[520px]  my-5">
+            <div className="w-64 h-64">
+              <Image src={ch} className="w-64 h-64" alt="Shoes" />
+            </div>
+            <div>
+              <div className="flex w-64 h-64 justify-between">
+                <div className="ml-2">
+                  <h5>Grilled Beef <br /> with potatoes</h5> 
+                </div>
+                <div><h5 style={{color:"#c8a97e"}}> $29 </h5></div>
+              </div>
+            </div>
+            </div>
              <div className="card w-72  bg-base-100 shadow-xl  ">
   <Image src={ch} className="w-72 h-72" alt="Shoes" />
   <div className="card-body">
