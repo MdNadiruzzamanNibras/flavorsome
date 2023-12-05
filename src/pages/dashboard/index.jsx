85ca3f12a48178c2ad5069c226af11faac3731b5
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-05 15:34:32 +0600

    add review

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,7 +1,13 @@
 import DashBoard from "@/Component/layout/DashBoard";
+import useAdmin from "@/Hook/useAdmin";
+import auth from "@/firebase/firebase.config";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 
 const DashboardPage = () => {
+      const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user)
+    console.log(admin);
     return (
         <div>
             <h1>this is dashboard</h1>
