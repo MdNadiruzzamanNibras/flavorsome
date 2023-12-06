82f6a3b107c61f226abf1edad8f5835d9bb6f921
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-06 17:22:43 +0600

    some change

--- src/Component/Home/FoodPHILOSOPHY.js
+++ src/Component/Home/FoodPHILOSOPHY.js
@@ -1,36 +1,53 @@
-import Image from 'next/image';
-import first from '../../../public/images/philosphy/uper.jpg'
-import two from '../../../public/images/philosphy/down.jpg'
+import Image from "next/image";
+import first from "../../../public/images/philosphy/uper.jpg";
+import two from "../../../public/images/philosphy/down.jpg";
 
 const FoodPHILOSOPHY = () => {
-    return (
-        <div className='container mx-auto'>
-            <div className='mt-80 lg:mt-36'>
-            
-            <div className='grid grid-cols-1 lg:grid-cols-2  gap-2'>
-            <div className='lg:ml-72'>
-                <Image src={two} className="lg:w-72 lg:h-96  lg:relative " alt="food philosopy" />
-                 <Image src={first} className="lg:w-80 lg:h-[400px]  lg:absolute lg:ml-24 lg:mt-[-500px]" alt="food philosopy" />
-                
-               
-                
+  return (
+    <div className="container mx-auto">
+      <div className="mt-80 lg:mt-36">
+        <div className="grid grid-cols-1 lg:grid-cols-2  gap-2">
+          <div className="lg:ml-72 md:ml-16">
+            <Image
+              src={two}
+              className="md:w-72 md:h-96  md:relative "
+              alt="food philosopy"
+            />
+            <Image
+              src={first}
+              className="md:w-80 md:h-[400px]  md:absolute md:ml-24 md:mt-[-500px]"
+              alt="food philosopy"
+            />
+          </div>
+          <div>
+            <div className="md:ml-16 lg:ml-0 md:mt-7 lg:mt-0">
+              <div className="flex items-center ">
+                <h6 style={{ color: "#c8a97e" }} className="text-xl font-bold">
+                  QUALITY & BALANCE
+                </h6>
+                <hr
+                  style={{ backgroundColor: "#c8a97e" }}
+                  className="w-20 h-1 ml-8"
+                />
+              </div>
+              <h2 className="text-4xl my-6 font-bold">OUR FOOD PHILOSOPHY</h2>
+              <p className="text-lg mb-6">
+                Simple and balanced. Alexander Petillo brings together flavors
+                and specialties from Italy and beyond to create his own culinary
+                world, full of surprising artistry.
+              </p>
+              <button
+                style={{ backgroundColor: "#c8a97e" }}
+                className="uppercase py-3 px-6 text-white font-semibold"
+              >
+                Learn more
+              </button>
             </div>
-                <div >
-                    <div >
-                        <div className='flex items-center'>
-                            <h6 style={{color:"#c8a97e"}} className='text-xl font-bold'>QUALITY & BALANCE</h6>
-                            <hr style={{ backgroundColor: "#c8a97e" }} className='w-20 h-1 ml-8' />
-                        </div>
-                        <h2 className='text-4xl my-6 font-bold'>OUR FOOD PHILOSOPHY</h2>
-                        <p className='text-lg mb-6'>Simple and balanced. Alexander Petillo brings together flavors and specialties from Italy and beyond to create his own culinary world, full of surprising artistry.</p>
-                        <button style={{ backgroundColor: "#c8a97e" }} className='uppercase py-3 px-6 text-white font-semibold'>Learn more</button>
-                    </div>
-                    
-            </div>
+          </div>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FoodPHILOSOPHY;
--- src/Component/Home/Menu.js
+++ src/Component/Home/Menu.js
@@ -1,49 +1,56 @@
-
 import Image from "next/image";
 import Link from "next/link";
 
+const Menu = ({ menuData }) => {
+  return (
+    <div className="container mx-auto">
+      <div className="my-20  ">
+        <h1 className="text-4xl font-extrabold text-center my-7">Our Menu</h1>
 
-const Menu = ({menuData}) => {
-    return (
-      <div className="container mx-auto">
-         <div className="my-20  ">
-            <h1 className="text-4xl font-extrabold text-center my-7">Our Menu</h1>
-          
-             <div className="grid grid-cols-1 ml-24 lg:ml-0 md:grid-cols-2  gap-5 ">
-         
-            { 
-              menuData.slice(0, 4).map(food =>
-                <div key={food._id} className="  lg:flex lg:items-center rounded-lg w-64 border-2 lg:w-[520px]   my-5">
-            <div className="">
-              <Image width={1000} height={1000} src={food.image_url} className=" lg:w-64 lg:h-64" alt="menu" />
-            </div>
-            <div className="w-32 lg:w-64 lg:h-64">
-              <div className="flex  ljustify-between text-xl font-bold mt-3">
-                <div className="ml-2 ">
-                        <h5 >{food.title}</h5> 
-                        
-                </div>
-                <div><h5 style={{color:"#c8a97e"}}> $29 </h5></div>
+        <div className="grid grid-cols-1 ml-24 lg:ml-0 md:grid-cols-2  gap-5 ">
+          {menuData.slice(0, 4).map((food) => (
+            <div
+              key={food._id}
+              className="  lg:flex lg:items-center rounded-lg w-64 border-2 lg:w-[520px]   my-5"
+            >
+              <div className="">
+                <Image
+                  width={1000}
+                  height={1000}
+                  src={food.image_url}
+                  className=" lg:w-64 lg:h-64"
+                  alt="menu"
+                />
               </div>
-              <h4 className="text-lg mt-12 lg:mx-3">Meat, Potatoes, Rice, Tomatoe</h4>
-                    <div >
-                      <Link href={`food/${food?._id}`}>
-                      <button style={{backgroundColor:"#c8a97e", color:"white"}} className=" py-2 px-5 mt-3 ml-3 font-bold">Order Now</button>
-                    </Link>
-                    </div>
+              <div className="w-32 lg:w-64 lg:h-64">
+                <div className="flex w-32 lg:w-64 justify-between text-xl font-bold mt-3">
+                  <div className="ml-2 ">
+                    <h5>{food.title}</h5>
+                  </div>
+                  <div className="ml-28">
+                    <h5 style={{ color: "#c8a97e" }}> $29 </h5>
+                  </div>
+                </div>
+                <h4 className="text-lg mt-12 lg:mx-3">
+                  Meat, Potatoes, Rice, Tomatoe
+                </h4>
+                <div className="lg:ml-0 md:ml-16">
+                  <Link href={`food/${food?._id}`}>
+                    <button
+                      style={{ backgroundColor: "#c8a97e", color: "white" }}
+                      className=" py-2 px-5 mt-3 ml-3 font-bold"
+                    >
+                      Order Now
+                    </button>
+                  </Link>
+                </div>
+              </div>
             </div>
-            </div>
-                 
-
-               )
-         }
-             
-            
-       </div>
-            
-       </div>
-       </div>
-    );
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
