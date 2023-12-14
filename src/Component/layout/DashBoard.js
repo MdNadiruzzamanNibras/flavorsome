28ddae2fc70ba04cd1acedbfc787785f52971952
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-14 20:55:12 +0600

    some change dashboard layout

--- src/Component/layout/DashBoard.js
+++ src/Component/layout/DashBoard.js
@@ -31,85 +31,85 @@ const DashBoard = ({ children }) => {
         <div className="flex  flex-col h-full">
           <div className="flex-grow">
             <div className=" flex flex-col text-2xl text-start ml-10 mt-5">
-              <h1 className="mb-3 flex items-center">
-                <Link href="/dashboard">
+              <Link href="/dashboard">
+                <h1 className="mb-3 flex items-center">
                   <span className="text-3xl">
                     <MdOutlinePermIdentity />
                   </span>
                   <span className="ml-3">Profile</span>
-                </Link>
-              </h1>
+                </h1>
+              </Link>
 
               {user && !admin && (
                 <>
-                  <h1 className="my-3 flex items-center">
-                    <Link href="/dashboard/review">
+                  <Link href="/dashboard/review">
+                    <h1 className="my-3 flex items-center">
                       <span className="text-3xl">
                         <VscFeedback />
                       </span>
                       <span className="ml-3"> Review</span>
-                    </Link>
-                  </h1>
-                  <h1 className="my-3 flex items-center">
-                    <Link href="/dashboard/mybook">
+                    </h1>
+                  </Link>
+                  <Link href="/dashboard/mybook">
+                    <h1 className="my-3 flex items-center">
                       <span className="text-3xl">
                         <BsFillJournalBookmarkFill />
                       </span>
                       <span className="ml-3">Book Table</span>
-                    </Link>
-                  </h1>
-                  <h1 className="my-3 flex items-center">
-                    <Link href="/order/myorder">
+                    </h1>
+                  </Link>
+                  <Link href="/order/myorder">
+                    <h1 className="my-3 flex items-center">
                       <span className="text-3xl">
                         <MdNoFood />
                       </span>
                       <span className="ml-3">My Order</span>
-                    </Link>
-                  </h1>
+                    </h1>
+                  </Link>
                 </>
               )}
 
               {admin && (
                 <>
-                  <h1 className="my-3 flex items-center">
-                    <Link href="/dashboard/addfood">
+                  <Link href="/dashboard/addfood">
+                    <h1 className="my-3 flex items-center">
                       <span className="text-3xl">
                         {" "}
                         <BiSolidFoodMenu />
                       </span>
                       <span className="ml-3"> Add Food</span>
-                    </Link>
-                  </h1>
-                  <h1 className="my-3 flex items-center">
-                    <Link href="/dashboard/book">
+                    </h1>
+                  </Link>
+                  <Link href="/dashboard/book">
+                    <h1 className="my-3 flex items-center">
                       <span className="text-3xl">
                         <PiNotebookDuotone />
                       </span>
                       <span className="ml-3"> Book Table</span>
-                    </Link>
-                  </h1>
-                  <h1 className="my-3 flex items-center">
-                    <Link href="/order">
+                    </h1>
+                  </Link>
+                  <Link href="/order">
+                    <h1 className="my-3 flex items-center">
                       <span className="text-3xl">
                         <LuClipboardList />
                       </span>
                       <span className="ml-3">All Order</span>
-                    </Link>
-                  </h1>
+                    </h1>
+                  </Link>
                 </>
               )}
             </div>
           </div>
           <div className="flex-none">
             <div className=" flex mt-auto flex-col text-2xl text-start ml-10 ">
-              <h1 className="my-3 flex items-center">
-                <Link href="/">
+              <Link href="/">
+                <h1 className="my-3 flex items-center">
                   <span className="text-3xl">
                     <FaHome />
                   </span>
                   <span className="ml-3">Home</span>
-                </Link>
-              </h1>
+                </h1>
+              </Link>
               <h1
                 onClick={logout}
                 className="mt-3 mb-32 flex items-center cursor-pointer "
