97ded90be435005d696b500f8130ab420ffb537a
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2024-01-01 21:40:25 +0600

    some change in counter

--- src/Component/Home/CountAll.js
+++ src/Component/Home/CountAll.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { CountUp } from "use-count-up";
 import count from "../../../public/images/couter/Process-bg.png";
-import "../../styles/styles.css";
+
 const CountAll = () => {
   const counterName = [{}];
   return (
@@ -25,43 +25,42 @@ const CountAll = () => {
         />
       </div>
 
-      <h1 className="text-8xl  absolute w-full  bg-white text-center">
-        Hello world
-      </h1>
       {/* className="absolute z-10 mt-[-500px] lg:mt-[-200px]" */}
 
-      <div
-        className="grid background-image  text-center
-         text-white grid-cols-12   mb-4 bg-red-600"
-      >
-        <div className=" mt-5 col-span-12 md:col-span-6 lg:col-span-3 ">
-          <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
-            <CountUp isCounting start={0} end={18} duration={13} />
-          </h1>
-          <p className="lg:font-medium lg:text-base uppercase">
-            Years of experienced
-          </p>
-        </div>
+      <div className="absolute  mt-[-500px] lg:mt-[-200px]">
+        <div
+          className="grid  text-center
+         text-white grid-cols-12   mb-4 "
+        >
+          <div className=" mt-5 col-span-12 text-center lg:px-14  md:col-span-6 lg:col-span-3 ">
+            <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
+              <CountUp isCounting start={0} end={18} duration={13} />
+            </h1>
+            <p className="lg:font-medium lg:text-base uppercase">
+              Years of experienced
+            </p>
+          </div>
 
-        <div className="mt-5 col-span-12 md:col-span-6 lg:col-span-3">
-          <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
-            <CountUp isCounting start={0} end={100} duration={13} />
-          </h1>
-          <p className="lg:font-medium lg:text-base">MENUS/DISH</p>
-        </div>
+          <div className="mt-5 col-span-12 text-center  lg:px-14 md:col-span-6 lg:col-span-3">
+            <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
+              <CountUp isCounting start={0} end={100} duration={13} />
+            </h1>
+            <p className="lg:font-medium lg:text-base">MENUS/DISH</p>
+          </div>
 
-        <div className="mt-5 col-span-12 md:col-span-6 lg:col-span-3">
-          <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
-            <CountUp isCounting start={0} end={50} duration={13} />
-          </h1>
-          <p className="lg:font-medium lg:text-base">STAFFS</p>
-        </div>
+          <div className="mt-5 col-span-12 text-center lg:px-14 md:col-span-6 lg:col-span-3">
+            <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
+              <CountUp isCounting start={0} end={50} duration={13} />
+            </h1>
+            <p className="lg:font-medium lg:text-base">STAFFS</p>
+          </div>
 
-        <div className="mt-5 col-span-12 md:col-span-6 lg:col-span-3">
-          <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
-            <CountUp isCounting start={5000} end={15000} duration={12} />
-          </h1>
-          <p className="lg:font-medium lg:text-base">HAPPY CUSTOMERS</p>
+          <div className="mt-5 col-span-12 lg:px-14 md:col-span-6 lg:col-span-3">
+            <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
+              <CountUp isCounting start={5000} end={15000} duration={12} />
+            </h1>
+            <p className="lg:font-medium lg:text-base">HAPPY CUSTOMERS</p>
+          </div>
         </div>
       </div>
     </div>
