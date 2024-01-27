463ad548a63a68499da5f67f4d4d661eaf973033
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2024-01-27 22:21:44 +0600

    some chang

--- src/Component/Home/CountAll.js
+++ src/Component/Home/CountAll.js
@@ -27,12 +27,19 @@ const CountAll = () => {
 
       {/* className="absolute z-10 mt-[-500px] lg:mt-[-200px]" */}
 
-      <div className="absolute ml-20  mt-[-500px] lg:ml-10 lg:mt-[-200px]">
+      <div
+        style={{
+          backgroundImage: url(${'../../../public/images/couter/Process-bg.png'}),
+          width: "100%",
+          height: "100%",
+        }}
+        className=""
+      >
         <div
           className="grid  text-center
          text-white grid-cols-12   mb-4 "
         >
-          <div className=" mt-5 col-span-12 text-center lg:px-14  md:col-span-6 lg:col-span-3 ">
+          <div className=" mt-5 col-span-12 text-center lg:px-14   md:col-span-3 ">
             <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
               <CountUp isCounting start={0} end={18} duration={13} />
             </h1>
@@ -41,21 +48,21 @@ const CountAll = () => {
             </p>
           </div>
 
-          <div className="mt-5 col-span-12 text-center  lg:px-14 md:col-span-6 lg:col-span-3">
+          <div className="mt-5 col-span-12 text-center  lg:px-14  md:col-span-3">
             <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
               <CountUp isCounting start={0} end={100} duration={13} />
             </h1>
             <p className="lg:font-medium lg:text-base">MENUS/DISH</p>
           </div>
 
-          <div className="mt-5 col-span-12 text-center lg:px-14 md:col-span-6 lg:col-span-3">
+          <div className="mt-5 col-span-12 text-center lg:px-14  md:col-span-3">
             <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
               <CountUp isCounting start={0} end={50} duration={13} />
             </h1>
             <p className="lg:font-medium lg:text-base">STAFFS</p>
           </div>
 
-          <div className="mt-5 col-span-12 lg:px-14 md:col-span-6 lg:col-span-3">
+          <div className="mt-5 col-span-12 lg:px-14  md:col-span-3">
             <h1 className="text-6xl lg:text-8xl text-stone-300 font-bold">
               <CountUp isCounting start={5000} end={15000} duration={12} />
             </h1>
